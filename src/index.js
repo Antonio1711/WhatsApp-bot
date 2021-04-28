efea6d1ff4094cb7ecc90e8c258376030e9f860d
Antonio1711 <71889113+Antonio1711@users.noreply.github.com>
2021-04-28 13:56:18 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const puppeteer = require("puppeteer");
+
+// Login Function Logic
+(async function main() {
+  try {
+    // Configures puppeteer
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.setUserAgent(
+      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
+    );
+
+    //Navigates to Whatsapp
+    await page.goto("https://web.whatsapp.com/");
+    await page.waitForSelector("._1C2Q3");
+    await delay(5000);
+
+    //Change to contact you want to send messages to
+    const contactName = "Amigo";
+    await page.click(`span[title='${contactName}']`);
+    await page.waitForSelector("._2A8P4");
+
+    //Finds the message bar and focuses on it
+    const editor = await page.$("._2A8P4");
+    await editor.focus();
+
+    //Amount of messages you want to send
+    const amountOfMessages = 5;
+
+    //Loops through cycle of sending message
+    for (var i = 0; i < amountOfMessages; i++) {
+      await page.evaluate(() => {
+        const message = "E aí, tudo certo?";
+        document.execCommand("insertText", false, message);
+      });
+      await page.click("span[data-testid='send']");
+      await delay(500);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
+function delay(time) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, time);
+  });
+}
